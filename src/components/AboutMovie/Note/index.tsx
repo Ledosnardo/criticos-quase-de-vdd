588d255f05a96649c20c67f6ec162a4fe343a8d3
Leonardo <ledosnardo@gmail.com>
2023-09-07 20:13:22 -0300

    New comments system of critics

--- src/components/AboutMovie/Note/index.tsx
+++ src/components/AboutMovie/Note/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { UseColorNote } from "../../../hooks/UseColorNote";
 import { IMovie } from "../../../types/IMovie";
 import ContainerCritic from "../../ContainerCritic";
@@ -7,8 +8,17 @@ interface props{
     movie: IMovie
 }
 
+interface critic {
+    name: string
+    note: number
+}
+
+
 const Note = ({ movie }: props) => {
     const color = UseColorNote(movie.note);
+    const  numberSort = Math.floor(Math.random() * movie.assessments.length - 1)
+    const [ chosen, setChosen ] = useState<critic>(movie.assessments[numberSort]);
+    
 
     return(
         <section className={styles.section_note}>
@@ -19,14 +29,22 @@ const Note = ({ movie }: props) => {
             </div>
             <div className={styles.critic}>
                 <div className={styles.avaliation}>
-                    <div>
+                    <ContainerCritic critic={chosen} />
+                    <div className={styles.avaliation__text}>
                         <h3 className={styles.text}>{movie.avaliation}</h3>
                     </div>
                 </div>
                 <div className={styles.criticsContainer}>
-                    {movie.assessments.map((critic, index) => (
-                        <ContainerCritic critic={critic} key={index} />
-                    ))}
+                    {movie.assessments.map((critic, index) => {
+                        if(critic.name == chosen.name){
+                            return '';
+                        } 
+                        return (
+                            <div onClick={() => setChosen(movie.assessments[index])}>
+                                <ContainerCritic critic={critic} key={index}/>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </section>
